522d6e22473507b3d523b7cb4af5d7791df112cf
viettran96 <72445198+viettran96@users.noreply.github.com>
2020-10-27 19:36:20 +0700

    Create BTVN1.cpp

---
+++ BTVN1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	
+	if(2<=a<8){
+		printf("day la thu %d",a);
+}else{
+	printf("khong phai ngay trong tuan");
+}if(a==8){
+		printf("day la chu nhat");
+			}
+	}
